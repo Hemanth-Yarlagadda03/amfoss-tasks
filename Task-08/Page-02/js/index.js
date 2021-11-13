a83d20f46800124710964e59f5016d6eb7f92b50
Hemanth Yarlagadda <yhlakshman@gmail.com>
2021-11-13 16:23:07 +0530

    Create index.js

---
+++ Task-08/Page-02/js/index.js
@@ -0,0 +1,39 @@
+function myFunction(){
+
+    var today = new Date();
+
+    var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
+
+    var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+
+    var dateTime = date+' '+time;
+
+}
+function changeColor(color){
+    document.body.style.background = '#'+Math.floor(Math.random()*16777215).toString(16);
+}
+
+function reload(){
+    window.location.reload()
+ }
+function openWin(){
+    window.open("./index.html");
+}
+function consoles()
+{
+console.log("I did it")
+}
+
+
+function nextPage(){
+    location.href = "http://127.0.0.1:8080/page-3/chart.html";
+}
+function saveData(){
+    var input = document.getElementById("saveServer");
+    localStorage.setItem("server", input.value);
+    var storedValue = localStorage.getItem("server");
+    console.log(storedValue)
+}
+function sillystuff(){
+    location.href = "https://www.youtube.com/watch?v=dQw4w9WgXcQ";
+}
